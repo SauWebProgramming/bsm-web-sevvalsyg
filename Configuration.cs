311982c52a750cf58e420b7f4a22bdeea438a099
sevvalsyg <58918762+sevvalsyg@users.noreply.github.com>
2021-01-02 20:01:32 +0300

    Add files via upload

--- Configuration.cs
+++ Configuration.cs
@@ -1,18 +1,18 @@
-﻿namespace ISE309_ProjeOdev_B181200028_B181200039.Migrations
+﻿namespace ISE309_ProjeOdev_B181200039_B181200028.Migrations
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ISE309_ProjeOdev_B181200028_B181200039.Models.Sınıflar.Context>
+    internal sealed class Configuration : DbMigrationsConfiguration<ISE309_ProjeOdev_B181200039_B181200028.Models.Siniflar.Context>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(ISE309_ProjeOdev_B181200028_B181200039.Models.Sınıflar.Context context)
+        protected override void Seed(ISE309_ProjeOdev_B181200039_B181200028.Models.Siniflar.Context context)
         {
             //  This method will be called after migrating to the latest version.
 
