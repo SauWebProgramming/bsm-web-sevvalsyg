d6acbba5ca318f7e220944755d806652d94ff96b
b181200028 <75884559+b181200028@users.noreply.github.com>
2021-01-02 20:00:52 +0300

    Add files via upload

---
+++ AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ISE309_ProjeOdev_B181200028_B181200039.Models.Sınıflar;
+
+namespace ISE309_ProjeOdev_B181200028_B181200039.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Hakkimizdas.ToList();
+            return View(degerler);
+        }
+    }
+}
