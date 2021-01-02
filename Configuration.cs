2184da47b89ff7ed1cc0c804a94e5126c226c00c
b181200028 <75884559+b181200028@users.noreply.github.com>
2021-01-02 20:01:31 +0300

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ISE309_ProjeOdev_B181200028_B181200039.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ISE309_ProjeOdev_B181200028_B181200039.Models.Sınıflar.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ISE309_ProjeOdev_B181200028_B181200039.Models.Sınıflar.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
